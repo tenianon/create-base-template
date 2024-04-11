43e34e5939e69dd5a9bf2d3895052abdea3fc015
nian <tenianon@gmail.com>
2024-04-11 09:32:55 +0800

    add template i18n

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+  {
+    rules: {
+
+    'no-var': 'off',
+    'prefer-const': 'off',
+      
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'warn',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+]
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export const log = (...args) => {
-  console.log(123);
-};
+import path from 'path'
+import prompts from 'prompts'
+import { language } from './i18n/index'
+;(async () => {
+  const a = '1'
+  const x = 2
+  const locale = Intl.DateTimeFormat().resolvedOptions().locale
+
+  const currentDir = path.resolve('.')
+  const isRootDir = currentDir === path.parse(currentDir).root
+
+  const response = await prompts({
+    type: 'text',
+    name: 'projectName',
+    message: language[locale]
+      ? language[locale].qProjectName
+      : language['en-US'].qProjectName,
+    validate: async (value) => {
+      if (value === '.') {
+        if (!isRootDir) {
+          return true
+        } else {
+          return language[locale]
+            ? language[locale].eProjectName1
+            : language['en-US'].eProjectName1
+        }
+      } else if (value.match(/^[a-zA-Z0-9-_]+$/)) {
+        return true
+      } else {
+        return language[locale]
+          ? language[locale].eProjectName2
+          : language['en-US'].eProjectName2
+      }
+    },
+  })
+
+  console.log(response)
+})()
